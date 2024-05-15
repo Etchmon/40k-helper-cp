264bef5e7e799295ad668a6415715ab7bea28509
James Etchells <Etch.rush@gmail.com>
2024-05-15 12:19:01 -0700

    add: button to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
+import { Button } from "./components/button";
 
 function App() {
   const navigate = useNavigate();
@@ -15,10 +16,12 @@ function App() {
         <h1 className="landing-header">
           In the grim darkness of the far future there is only war...
         </h1>
-        <button className="start" onClick={startGame}>
-          New Game
-        </button>
       </header>
+      <main>
+        <Button variant="default" size="default" onClick={startGame}>
+          Start Game
+        </Button>
+      </main>
     </div>
   );
 }
