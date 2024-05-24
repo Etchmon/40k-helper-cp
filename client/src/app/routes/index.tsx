a7e9e52ea5670db0dddd402f53256fa46f53ff6f
James Etchells <Etch.rush@gmail.com>
2024-05-24 14:43:12 -0700

    add: landing page

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,4 +1,15 @@
-import { QueryClient } from "@tanstack/react-query";
+// import { QueryClient } from "@tanstack/react-query";
 import { createBrowserRouter } from "react-router-dom";
 
 // protected routes
+
+export const createRouter = () =>
+  createBrowserRouter([
+    {
+      path: "/",
+      lazy: async () => {
+        const { LandingRoute } = await import("./landing");
+        return { Component: LandingRoute };
+      },
+    },
+  ]);
---
+++ client/src/app/routes/landing.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router";
+
+import { Button } from "@/components/ui/button";
+
+export const LandingRoute = () => {
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    if (localStorage.getItem("token")) {
+      navigate("/game");
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="landing-header">
+          In the grim darkness of the far future there is only war...
+        </h1>
+      </header>
+      <main>
+        <Button variant="default" size="default" onClick={handleStart}>
+          Start Game
+        </Button>
+      </main>
+    </div>
+  );
+};
