1056f947a731cfc1c6ce31d1b1558b587db7b57d
James Etchells <Etch.rush@gmail.com>
2024-05-31 21:53:51 -0700

    ADD: image to landing page

--- client/src/app/routes/landing.tsx
+++ client/src/app/routes/landing.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router";
 
 import { Head } from "@/components/seo/head";
 import { Button } from "@/components/ui/button";
+import backgroundImage from "@/assets/40k-battle01.jpg";
 
 export const LandingRoute = () => {
   const navigate = useNavigate();
@@ -19,8 +20,12 @@ export const LandingRoute = () => {
   return (
     <>
       <Head title="Landing" />
-      <div className="flex items-center h-screen bg-slate-950">
-        <div className="mx-auto text-center px-4 py-12 sm:px-6 lg:px-8 lg:py-12">
+      <div
+        className="relative flex items-center h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="absolute inset-0 opacity-50 bg-slate-950"></div>
+        <div className="mx-auto text-center px-4 py-12 sm:px-6 lg:px-8 lg:py-12 z-10">
           <h2 className="text-3xl font-extrabold text-slate-50 sm:text-4xl">
             <span className="block">
               Warhammer 40k Combat Patrol Digital Interface
